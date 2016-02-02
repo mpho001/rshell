1c3fd85883e959e1b6e9879cdc8e2bfdf3112ade
Niharika Battula <nbatt002@hammer.cs.ucr.edu>
2016-02-02 13:14:19 -0800

    add simple class to Shell

--- src/Shell.h
+++ src/Shell.h
@@ -5,14 +5,55 @@
 
 using namespace std;
 
+// our main abstract base class, used main for pointers
+
 class Shell {
     public:
         // Constructor and destructor
         Shell();
         ~Shell();
-        virtual void run() = 0;
 };
 
+class Execute: public Shell {
+	public:
+		// this function would be used to execute our rshell
+	        void execute();
+};
 
+class Quit: public Shell {
+	public:
+		// this function would allow us to exit our rshell
+		void quit();
+};
+
+class Run: public Shell {
+	public: 
+		//this function we would call to run our rshell
+		void run();
+};
+
+class Connectors: public Shell {
+	public: 
+		// this we would lead into our connectors and try to identify th                //  m
+		virtual bool behave() = 0;
+};
+
+class And: public Connectors {
+	public:
+		// this would identify if the connector is and
+		bool behave();
+};
+
+class Semi: public Connectors {
+	public:
+		// this would identify if the connector is semi 	
+		bool behave();
+};
+
+class Or: public Connectors {
+	public:
+		// this would identify if the connector is or
+		bool behave();
+};
 #endif
 
