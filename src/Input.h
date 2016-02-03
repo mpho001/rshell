9e9c9caf0c44ad9664e7ef63b68bc5cb1aa31b27
Melinda <mpho001@ucr.edu>
2016-02-02 21:42:24 -0800

    recognizes semicolon

--- src/Input.h
+++ src/Input.h
@@ -16,7 +16,6 @@ class Input {
         string strLine;
         // char* str;
         char* cmds[50];
-        char* connType[50];
         vector<string> wholeLine;
         queue<char** > work;
     public:
@@ -46,56 +45,86 @@ void Input::parse() {
     while (p != 0) {
         string temp = p;
         if (temp == "&&" || temp == "||" || temp == ";") {
+            // null terminated
+            cmds[i] = (char*)"\0";
             work.push(cmds);
             i = 0;
-            *cmds = new char[50];
+            // *cmds = new char[50];
+            memset(cmds, 0, sizeof(cmds));
             cmds[i] = p;
             ++i;
+            cmds[i] = (char*)"\0";
             work.push(cmds);
             i = 0;
-            *cmds = new char[50];
+            memset(cmds, 0, sizeof(cmds));
             single = false;
         }
 
-        // cout << p << endl;
         else {
             // do semicolon check
             // FIX the string
             if (temp.at(temp.size()-1) == ';') {
-                cmds[i] = (char*)";";
-                work.push(cmds);
-                *cmds = new char[50];
-                i = 0;
+                //cmds[i] = (char*)";";
+                //if (i != 0) {
+                //    work.push(cmds);
+                //    memset(cmds, 0, sizeof(cmds));
+                //    i = 0;
+                //}
+
+                //cmds[i] = (char*)";";
+                //work.push(cmds);
+                //memset(cmds, 0, sizeof(cmds));
+                //i = 0;
+
                 temp.erase(temp.size()-1);
                 cout << "temp: " << temp << endl;
-                char test[temp.size()+1];
-                strcpy(test, temp.c_str());
-                cmds[i] = test;
+    
+                char arr[1024];
+                strncpy(arr, temp.c_str(), sizeof(arr));
+                arr[sizeof(arr) - 1] = 0;
+                cmds[i] = arr;
                 ++i;
+                cmds[i] = (char*)"\0";
+                work.push(cmds);
+                memset(cmds, 0, sizeof(cmds));
+                i = 0;
+
+                cmds[i] = (char*)";";
+                ++i;
+                cmds[i] = (char*)"\0";
+                work.push(cmds);
+                memset(cmds, 0, sizeof(cmds));
+                i = 0;
+                single = false;
             }
             else {
                 cmds[i] = p;
                 ++i;
+                single = true;
             }
-            single = true;
+            //single = true;
         }
 
         p = strtok(NULL, delimit);
     }
 
     if (single) {
+        cmds[i] = (char*)"\0";
         work.push(cmds);
     }
 
     cout << "work size: " << work.size() << endl;
        
     while (work.size() != 0) {
-        char** a = work.front();
-        cout << *a << ' ';
+         char** a = work.front();
+         int k = 0;
+         while (a[k] != "\0") {
+             cout << a[k] << ' ';
+             ++k;
+         }
+        // cout << *a << ' ';
         work.pop();
-        cout << "up: " << work.size() << endl;
     }
-
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include "Shell.h"
 #include "Input.h"
 
@@ -10,6 +11,23 @@ int main() {
     in.getInput();
     in.parse(); cout << endl;
 
+    queue<char** > test;
+    char* a[3];
+    a[0] = (char*)"hello there";
+    a[1] = (char*)"omg";
+    a[2] = (char*)"\0";
+
+    test.push(a);
+    int i = 0;
+    while (test.size() != 0) {
+        char** b = test.front();
+        while (b[i] != "\0") {
+//            cout << b[i] << ' ';
+            ++i;
+        }
+        test.pop();
+    }
+//    cout << endl;
 
 
     return 0;
