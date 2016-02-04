aec7fbc47f64401e68b1ccf68d8ecccf86a465e3
Melinda <mpho001@ucr.edu>
2016-02-04 15:33:44 -0800

    convert to char function

--- src/Input.h
+++ src/Input.h
@@ -18,141 +18,35 @@ class Input {
         // char* str;
         char* cmds[50];
         vector<string> wholeLine;
-        queue<char** > work;
         queue<string> tasks;
     public:
         Input() {};
         void getInput();
-        void parse();
-        void Parse();
+        queue<string> Parse();
+        char** toChar(string a);
 };
 
+// if # spotted, cuts off # and everything after it
 void Input::getInput() {
     getline (cin, strLine);
+    if (strLine == "exit") {
+        cout << "user chose to exit" << endl;
+        exit(0);
+    }
     size_t found = strLine.find('#');
     if (found != string::npos) {
-        cout << "got the #" << endl;
         strLine.erase(strLine.begin() + found, strLine.end());
-        cout << strLine << endl;
-    }
-    else {
-        cout << "tough, son" << endl;
     }
 }
 
-// make string into array
-// breaks up line and stores tokens
-void Input::parse() {
-
-    char delimit[] = {' '};
-
-    char str[strLine.size()+1];
-    strcpy(str, strLine.c_str());
-    char* p = strtok(str, delimit);
-    
-    // keeps track of location in arrays
-    int i = 0;
-    // is it last part of line
-    bool single = true;
-    while (p != 0) {
-        string temp = p;
-        if (temp == "&&" || temp == "||" || temp == ";") {
-            // null terminated
-            //cmds[i] = (char*)"\0";
-            //work.push(cmds);
-            //i = 0;
-            //memset(cmds, 0, sizeof(cmds));
-            //cmds[i] = p;
-            //++i;
-            //cmds[i] = (char*)"\0";
-            //work.push(cmds);
-            //i = 0;
-            //memset(cmds, 0, sizeof(cmds));
-            single = false;
-        }
-
-        else {
-            // do semicolon check
-            // FIX when semicolon at the end
-            // it gives segfault
-            if (temp.at(temp.size()-1) == ';') {
-                //if (i != 0) {
-                //    work.push(cmds);
-                //    memset(cmds, 0, sizeof(cmds));
-                //    i = 0;
-                //}
-
-                //cmds[i] = (char*)";";
-                //work.push(cmds);
-                //memset(cmds, 0, sizeof(cmds));
-                //i = 0;
-
-                temp.erase(temp.size()-1);
-    
-                char arr[1024];
-                strncpy(arr, temp.c_str(), sizeof(arr));
-                arr[sizeof(arr) - 1] = 0;
-                cmds[i] = arr;
-                ++i;
-                cmds[i] = (char*)"\0";
-                work.push(cmds);
-                memset(cmds, 0, sizeof(cmds));
-                i = 0;
-
-                cmds[i] = (char*)";";
-                ++i;
-                cmds[i] = (char*)"\0";
-                work.push(cmds);
-                memset(cmds, 0, sizeof(cmds));
-                i = 0;
-                single = false;
-            }
-            // no semicolon at the end
-            else {
-                cmds[i] = p;
-                cout << "just word: ";
-                cout << cmds[i] << endl;
-                ++i;
-                single = true;
-            }
-            //single = true;
-        }
-
-        p = strtok(NULL, delimit);
-    }
-
-    if (single) {
-        cmds[i] = (char*)"\0";
-        work.push(cmds);
-    }
-
-    cout << "work size: " << work.size() << endl;
-       
-    while (work.size() != 0) {
-         char** a = work.front();
-         int k = 0;
-         while (a[k] != "\0") {
-             cout << a[k] << ' ';
-             ++k;
-         }
-        work.pop();
-    }
-}
-
-void Input::Parse() {
+queue<string> Input::Parse() {
     istringstream iss(strLine);
-
 
     // iss >> ws;
     string token;
     string cmd;
     bool end = true;
     while (getline(iss, token, ' ')) {
-        if (token == " ") {
-            cout << "here yo" << endl;
-            exit(0);
-        }
-
         if (token == "&&" || token == "||" || token == ";") {
             tasks.push(cmd);
             cmd.clear();
@@ -195,11 +89,25 @@ void Input::Parse() {
     }
 
     cout << "tasks size: " << tasks.size() << endl;
-    while (tasks.size() != 0) {
-        cout << tasks.front() << endl;
-        tasks.pop();
-    }
+    // while (tasks.size() != 0) {
+    //     cout << tasks.front() << endl;
+    //     tasks.pop();
+    //}
+    return tasks;
+}
 
+char** Input::toChar(string a) {
+    stringstream stream(a);
+    string oneWord;
+    int i = 0;
+    char** c = new char*[50];
+    while (stream >> oneWord) {
+        char* ptr = new char[oneWord.size() + 1];
+        memcpy(ptr, oneWord.c_str(), oneWord.size() + 1);
+        c[i] = ptr;
+        ++i;
+    }
+    return c;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,21 @@ int main() {
     Input in;
 
     in.getInput();
-   // in.parse(); cout << endl;
-    in.Parse();
+    queue<string> tasks = in.Parse();
 
-    queue<char** > test;
-    char* a[3];
-    a[0] = (char*)"hello there";
-    a[1] = (char*)"omg";
-    a[2] = (char*)"\0";
-
-    char* b[3];
-    b[0] = (char*)"poo";
-    b[1] = (char*)"uh";
-    b[2] = (char*)"\0";
-
-    test.push(a);
-    test.push(b);
-    int i = 0;
-    while (test.size() != 0) {
-        char** c = test.front();
-        i = 0;
-        while (c[i] != "\0") {
-//            cout << c[i] << ' ';
-            ++i;
-        }
-        test.pop();
+    char** c;
+    while (tasks.size() != 0) {
+        c = in.toChar(tasks.front());
+        tasks.pop();
     }
-//    cout << endl;
 
+    int i = 0;
+    while (c[i] != '\0') {
+        cout << c[i] << endl;
+        ++i;
+    }
+
+    delete c;
 
     return 0;
 
