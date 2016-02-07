720b5ac35a0106ea37c420d94e86597d52e448cb
Melinda <mpho001@ucr.edu>
2016-02-06 20:00:07 -0800

    moved execute into loop

--- src/main.cpp
+++ src/main.cpp
@@ -19,26 +19,15 @@ int main() {
     queue<string> tasks = in.Parse();
 
     char** c;
+    Execute ex;
     while (tasks.size() != 0) {
         c = in.toChar(tasks.front());
+        ex.execute(c);
         tasks.pop();
     }
 
-    pid_t pid = fork();
-    if (pid == 0) {
-        // cout << "child: " << pid << endl;
-        if (execvp(c[0], c) == -1) {
-            perror("exec");
-            exit(0);
-        }
-    }
-
-    if (pid > 0) {
-        if ( wait(0) == 1 ) {
-            perror("wait");
-        }
-        // cout << "parent: " << pid << endl;
-    }
+    // Execute ex;
+    // ex.execute(c);
 
 //    int i = 0;
 //    while (c[i] != '\0') {
@@ -51,7 +40,7 @@ int main() {
     cout << flush;
     delete c;
 
- //   }
+//    }
 
     return 0;
 
