cd50fbd04c9d3f53dcaa0779a79275cb5d11b29d
Melinda <mpho001@ucr.edu>
2016-02-10 10:03:04 -0800

    added new test cases

--- tests/single_command.sh
+++ tests/single_command.sh
@@ -3,5 +3,23 @@ cd ..
 cd src
 
 echo echo hello | ./a.out
-echo echo bash bash bash | ./a.out
-echo "echo hello && echo second" | ./a.out
+echo echo how are    you  doing this wonderful day | ./a.out
+echo ls | ./a.out
+echo ls -a | ./a.out
+printf "\n"
+echo "invalid commands"
+echo fake | ./a.out
+echo invalid | ./a.out
+echo ech oops | ./a.out
+printf "\n"
+echo "date and mkdir"
+echo date | ./a.out
+echo mkdir test folder | ./a.out
+echo "no space"
+echo "" | ./a.out
+echo "one space"
+echo " " | ./a.out
+echo "two spaces"
+echo "  " | ./a.out
+echo "                                       " | ./a.out
+echo "exit" | ./a.out
