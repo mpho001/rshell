e99aeb89de1f87257b760c16902035f83c9d7dec
Niharika Battula <nbatt002@hammer.cs.ucr.edu>
2016-02-11 20:27:35 -0800

    add headers to Connectors/testing push

--- src/Connectors.cpp
+++ src/Connectors.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
+#include "Input.h"
+#include "Shell.h"
+#include <queue>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pwd.h>
 
-watsup melinda`
+using namespace std
+
+
