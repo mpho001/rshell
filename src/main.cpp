3effcb0e4965985a18e80940c165a2b6040fdafb
Melinda <mpho001@ucr.edu>
2016-02-12 12:36:22 -0800

    cleaned code, commented out other executes

--- src/main.cpp
+++ src/main.cpp
@@ -16,90 +16,101 @@ int main() {
     
     while(1) {
 
-    in.getInput();
-    tasks = in.Parse();
+        in.getInput();
+        tasks = in.Parse();
 
-    bool comp_status;
-    char** c = NULL;
-    Execute ex;
-    //bool worked = true;
-    while (tasks.size() != 0) 
-    {
-	
-	// if at any point the user has exit
-        if (tasks.front() == "exit") {
-            exit(0);
-        }
-
-//------------------------------------------------------------------------------------------------------------
-
-        else if (tasks.front() == "&&")
-       	{
-	    tasks.pop(); //gets rid of the &&
-            // if the command before this one does returns false then we dont do            //the next command
-	    if(comp_status == false)
-	    {
-		    cout << "went into false " << endl;
-		  // so the && connector should have already been deleted, this is a while loop to delete the next commands so that they wont run if the command before fails  
-		  while(tasks.front() != "&&" || tasks.front() != "||" || tasks.front() != ";")
-		  {
-			 tasks.pop();
-		  }
-	    }
-	
-	    // here we just run the next command like normal
-	    if(comp_status == true)
-	    {
-		    cout << "went into true" << endl;
-		    c = in.toChar(tasks.front());
-		    ex.execute(c, comp_status);
-		    tasks.pop();
-	    }
-        }
-
-//------------------------------------------------------------------------------------------------------------------
-
-        else if (tasks.front() == "||") 
-	{
-		tasks.pop();  // gets rid of the || 
-		if(comp_status == true)
-		{
-			if(tasks.size() != 0)
-		  	{
-			  	if (tasks.front() != "&&" || tasks.front() != "||" || tasks.front() != ";")
-			  	{
-				 	tasks.pop();
-			  	}
-		  	}
-		}
-		// if its false then for || it should run
-		
-		if(comp_status == false)
-		{
-	    	    cout << "in false" << endl;		
-		    c = in.toChar(tasks.front());
-		    ex.execute(c, comp_status);
-		    tasks.pop();
-		}
-	}
-
-//----------------------------------------------------------------------------------------------------------------------	
-
-       // else if (tasks.front() == ";") {
-        //    // do something else, son
-       // }
+        bool comp_status = true;
+        char** c = NULL;
+        Execute ex;
+        //bool worked = true;
+        while (tasks.size() != 0) 
+        {
         
-        // if not a connector or exit, then execute it 
-        else 
-	{
-            c = in.toChar(tasks.front());
-            ex.execute(c, comp_status);
-	    cout << "here " << comp_status << endl;
-	    tasks.pop();
-        }
-    }
+        // if at any point the user has exit
+            if (tasks.front() == "exit") {
+                exit(0);
+            }
 
-    cout << flush;
+    //-----------------------------------------------------------------------------------------
+
+            else if (tasks.front() == "&&")
+            {
+                tasks.pop(); //gets rid of the &&
+                // if the command before this one does returns false then we dont do            
+                // //the next command
+                if(comp_status == false)
+                {
+                    cout << "went into false " << endl;
+                    // so the && connector should have already been deleted, this is a while loop to
+                    // delete the next commands so that they wont run if the command before fails  
+                    while(tasks.front() != "&&" || tasks.front() != "||" || tasks.front() != ";")
+                    {
+                        tasks.pop();
+                    }
+                }
+        
+                // here we just run the next command like normal
+                if(comp_status == true)
+                {
+                    cout << "went into true" << endl;
+                    // c = in.toChar(tasks.front());
+                    // ex.execute(c, comp_status);
+                    // tasks.pop();
+                }
+            }
+
+    //---------------------------------------------------------------------------------------------
+
+            else if (tasks.front() == "||") 
+            {
+                tasks.pop();  // gets rid of the || 
+                if(comp_status == true)
+                {
+                    if(tasks.size() != 0)
+                    {
+                        if (tasks.front() != "&&" || tasks.front() != "||" || tasks.front() != ";")
+                        {
+                            tasks.pop();
+                        }
+                    }
+                }
+                // if its false then for || it should run
+            
+                if(comp_status == false)
+                {
+                    cout << "in false" << endl;		
+                    // c = in.toChar(tasks.front());
+                    // ex.execute(c, comp_status);
+                    // tasks.pop();
+                }
+        }
+
+    //-----------------------------------------------------------------------------------------------	
+
+           // else if (tasks.front() == ";") {
+            //    // do something else, son
+           // }
+            
+            // if not a connector or exit, then execute it 
+            else 
+        {
+                c = in.toChar(tasks.front());
+                ex.execute(c, comp_status);
+                int i = 0;
+                cout << "array: ";
+                while (c[i] != '\0') {
+                    cout << c[i] << ' ';
+                    ++i;
+                }
+                cout << endl;
+
+                // delete [] c;
+            // cout << "here " << comp_status << endl;
+            tasks.pop();
+            }
+        }
+
+        cout << flush;
     
     }
 
