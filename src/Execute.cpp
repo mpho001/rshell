3857657d25ba293096b44bd618da42fa17fe555e
Niharika Battula <nbatt002@hammer.cs.ucr.edu>
2016-02-12 17:13:20 -0800

    formatted execute.cpp

--- src/Execute.cpp
+++ src/Execute.cpp
@@ -12,37 +12,33 @@ using namespace std;
 void Execute::execute(char** a, bool &comp_status) {
 	// create a fork for a child and parent 
 	pid_t pid = fork();
-
+	
 	// the child 
-	if (pid < 0){
+	if (pid < 0) {
 		perror("fork failure");
 	}
-
-    if (pid == 0) 
-    {
-        // then its the child
-        if (execvp(a[0], a) == -1) 
-	{
-            perror("test");
-            exit(69);
-        }
-	else
-	{
-		exit(0);
+	
+	if (pid == 0) {
+		// then its the child
+		if (execvp(a[0], a) == -1) {
+			perror("test");
+			exit(69);
+		}
+		else{
+			exit(0);
+		}
 	}
-    }
-    int num = 0;
-    wait(&num);
-    
-    int num_2 = WEXITSTATUS(num);
-    
-    if(num_2 == 69)
-    {
+	
+	int num = 0;
+	wait(&num);
+	
+	int num_2 = WEXITSTATUS(num);
+	
+	if(num_2 == 69){
 	    comp_status = false;
-    }
-    if(num_2 == 0)
-    {
-	    comp_status = true;
-    }
-   
+	}
+	
+	if(num_2 == 0){
+		comp_status = true;
+	}
 }
