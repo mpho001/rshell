fa8b5b3dfd36d1a588137f435022b9d7bee68ed5
Melinda <mpho001@ucr.edu>
2016-02-12 21:57:10 -0800

    added new tests

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -11,3 +11,6 @@ echo "ls#-a" | ./a.out
 echo ls -a "#echo this" | ./a.out
 echo "#" | ./a.out
 echo date# "$$ random command" | ./a.out
+echo "date || ls || echo this #&& echo that" | ./a.out
+echo "echo hello || #echo world" | ./a.out
+echo "git bra && lsss #|| echo hey" | ./a.out
--- tests/exit.sh
+++ tests/exit.sh
@@ -5,3 +5,9 @@ cd ..
 cd src
 
 echo "exit" | ./a.out
+echo "echo hello && exit" | ./a.out
+echo "exit || echo hello" | ./a.out
+echo "echo this || exit" | ./a.out
+echo "date || echo hello && exit" | ./a.out
+echo "dat || exit" | ./a.out
+echo "     exit" | ./a.out
