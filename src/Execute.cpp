63451e790fd6f3b0c28f37753b786e64400d13ab
Niharika Battula <nbatt002@hammer.cs.ucr.edu>
2016-03-06 23:30:31 -0800

    made little comment changes to Execute

--- src/Execute.cpp
+++ src/Execute.cpp
@@ -28,12 +28,11 @@ void Execute::execute(char** a, bool &comp_status) {
             // new
 
 			perror("command not accomplished");
-			// we have exit 69 which would store the number
-			// 69 for later usage in determining comp_status
+			// we have errno which would store a number
+			// greater than 0 for later usage in determining comp_status
 			// also any number entered other than 0 to exit
 			// makes it return an unsuccessful completition 
-			// exit(69);
-            exit(errno);
+			exit(errno);
 		}
 		else{
 			// returns a successful completition
@@ -45,17 +44,14 @@ void Execute::execute(char** a, bool &comp_status) {
 	//tells the parent to wait
 	wait(&num);
 	
-	// when the exit 69 comes out of the if statement
+	// when the exit errno comes out of the if statement
 	// it is scrambled. so wexitstatus unscrambles it
 	// and puts it into num_2
 	int num_2 = WEXITSTATUS(num);
 	
-	// if num_2 is 69 then that means it was an 
+	// if num_2 is errno then that means it was an 
 	// unsuccessful completition and so comp_status 
 	// is false
-	//if(num_2 == 69){
-	//    comp_status = false;
-	//}
     
     // if errno > 0, then it didnt work
     if (num_2 > 0) {
@@ -63,7 +59,7 @@ void Execute::execute(char** a, bool &comp_status) {
     }
 	
 	// if num_2 is 0 then that means it was a 
-	// successfuk completition adn so comp_status
+	// successful completition and so comp_status
 	// is true 
 	if(num_2 == 0){
 		comp_status = true;
